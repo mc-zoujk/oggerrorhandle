fc506ca244bfe2abf8e2848da8aade140b472a2c
loet6010 <361198240@qq.com>
2016-02-18 15:39:24 +0800

    提交日志分析和获取表名两个类

---
+++ OggErrorHandle/src/com/mchz/errorhandle/implement/LogAnalyzeDispose.java
@@ -0,0 +1,78 @@
+package com.mchz.errorhandle.implement;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 
+ * @author liurh
+ * @date 2016年2月4日
+ * @intro 对行进行错误及类型判断
+ *
+ */
+public class LogAnalyzeDispose {
+	// 正则匹配
+	private Pattern pattern;
+	private Matcher matcher;
+	// 匹配字符串(表空间满）
+	private final static String MATCH_ORA_01654 = "(ORA-01654)";
+	// 匹配字符串(表空间不存在）
+	private final static String MATCH_ORA_00959 = "(ORA-00959)";
+	// 匹配字符串(分区表未打开行迁移）
+	private final static String MATCH_ORA_14402 = "(ORA-14402)";
+
+	/**
+	 * 分析错误类型
+	 * @param readLineTemp
+	 * @return
+	 */
+	public int getErrorType(String readLineTemp) {
+		// 表空间满匹配
+		pattern = Pattern.compile(MATCH_ORA_01654);
+		matcher = pattern.matcher(readLineTemp);
+		if (matcher.find())
+			return 1;
+
+		// 表空间不存在匹配
+		pattern = Pattern.compile(MATCH_ORA_00959);
+		matcher = pattern.matcher(readLineTemp);
+		if (matcher.find())
+			return 2;
+
+		// 分区表未打开行迁移
+		pattern = Pattern.compile(MATCH_ORA_14402);
+		matcher = pattern.matcher(readLineTemp);
+		if (matcher.find())
+			return 3;
+
+		return 0;
+	}
+
+	/**
+	 * 判断是否为待处理错误行
+	 * @param readLineTemp
+	 * @return
+	 */
+	public boolean isErrorLine(String readLineTemp) {
+		// 表空间满匹配
+		pattern = Pattern.compile(MATCH_ORA_01654);
+		matcher = pattern.matcher(readLineTemp);
+		if (matcher.find())
+			return true;
+
+		// 表空间不存在匹配
+		pattern = Pattern.compile(MATCH_ORA_00959);
+		matcher = pattern.matcher(readLineTemp);
+		if (matcher.find())
+			return true;
+
+		// 分区表未打开行迁移
+		pattern = Pattern.compile(MATCH_ORA_14402);
+		matcher = pattern.matcher(readLineTemp);
+		if (matcher.find())
+			return true;
+
+		return false;
+	}
+
+}
---
+++ OggErrorHandle/src/com/mchz/errorhandle/implement/TableNameExtractTool.java
@@ -0,0 +1,77 @@
+package com.mchz.errorhandle.implement;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 
+ * @author liurh
+ * @date   2016年2月18日
+ * @intro  将获取的表名、表空间名等字符串做进一步的提取，获取可用的表名、空间名等
+ *
+ */
+public class TableNameExtractTool {
+	private final static String MATCH_TBS_NAME = "(tablespace)[ ]+[^ ]+";
+	private final static String MATCH_TABLE_NAME = "(table)[ ]+[^ ]+";
+	private final static String MATCH_QUOTATION = "[\"|']";
+	private final static String MATCH_PERIOD = "[.]\\B";
+	
+	// 获取表空间名
+	public String getTbsName(String errorLine) {
+		Pattern pattern = Pattern.compile(MATCH_TBS_NAME);
+		Matcher matcher = pattern.matcher(errorLine);
+
+		if (matcher.find()) {
+			return getTableOrSpaceName(matcher.group(0));
+		}
+
+		return null;
+	}
+
+	// 获取表名方法
+	public String getTableName(String errorLine) {
+		Pattern pattern = Pattern.compile(MATCH_TABLE_NAME);
+		Matcher matcher = pattern.matcher(errorLine);
+
+		if (matcher.find()) {
+			return getTableOrSpaceName(matcher.group());
+		}
+
+		return null;
+	}
+	
+	/**
+	 * 根据字符串提取出表名或空间名等
+	 * @param  字符串格式(xxx AAAA),AAAA为需要提取内容
+	 * @return 提取出字段
+	 */
+	private String getTableOrSpaceName(String tempName) {
+		String extractName = null;
+		
+		System.out.println(tempName);
+		
+		// 去除字符串两边空格
+		tempName = tempName.trim();
+		
+		// 取得字符串最后一段
+		String[] mStringArr = tempName.split(" ");
+		extractName = mStringArr[mStringArr.length - 1];
+		
+		// 去除取得字段中的引号（单引号&双引号）
+		Pattern pattern = Pattern.compile(MATCH_QUOTATION);
+		Matcher matcher = pattern.matcher(extractName);
+		if (matcher.find()) {
+			extractName = matcher.replaceAll("");
+		}
+		
+		// 去除最后的句号
+		pattern = Pattern.compile(MATCH_PERIOD);
+		matcher = pattern.matcher(extractName);
+		if (matcher.find()) {
+			extractName = matcher.replaceAll("");
+		}
+		
+		return extractName;
+	}
+
+}
